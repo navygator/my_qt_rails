0f739bab01b109b190df6a5ff8d5f81f861f3dbd
Alexey Nureev <navygator@myopera.com>
2012-08-19 00:31:28 +0400

    Added user find

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
+    @user = User.from_omniauth(auth_hash)
     sign_in  @user
     redirect_to root_path
   end
