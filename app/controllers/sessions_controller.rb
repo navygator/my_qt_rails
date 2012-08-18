fb34107900f9cb08af4b05d2063ff90427865b28
Alexey Nureev <navygator@myopera.com>
2012-08-19 00:21:52 +0400

    Added signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
-    self.current_user = @user
+    sign_in  @user
     redirect_to root_path
   end
 
