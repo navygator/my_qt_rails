9c5d2871e9b7281901dfe4c66b27b14dafe9a5d8
Alexey Nureev <navygator@myopera.com>
2012-08-19 14:01:32 +0400

    Debug client foursquare

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,30 @@
 class UsersController < ApplicationController
+
+  before_filter :not_signed_user, :only => [:index, :show]
+  before_filter :correct_user, :only => [:edit]
+
   def index
   end
 
   def show
     @user = User.find(params[:id])
-    client = Foursquare2::Client.new(:oauth_token => @user.oauth_token)
-    @f_requests = client.user_requests
+    @client = Foursquare2::Client.new(:oauth_token => @user.oauth_token)
   end
 
   def edit
   end
 
-  def update
+  private
+  def signed_user
+    redirect_to root_path if signed_in?
+  end
+
+  def not_signed_user
+    redirect_to root_path, notice: "Please sign in" unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path, error: "You are not allowed" unless (current_user?(@user) || current_user.admin?)
   end
 end
