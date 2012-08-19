f0f52db80f92e54c7249a82302f53466f14788f1
Alexey Nureev <navygator@myopera.com>
2012-08-19 20:57:32 +0400

    Friends and Request base

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     provider "foursquare"
     sequence(:name)  { |n| "Person #{n}" }
-    uid "a14722f2031e73c5f00a8aad4d0cc31c73ece97f7e70ac40"
-    oauth_token "a14722f2031"
+    uid "35166090"
+    oauth_token "N2BJDFEOBQSNOGTGUXUDZOE502244WP24OLO0DBVHTSQOWEB"
   end
 end
